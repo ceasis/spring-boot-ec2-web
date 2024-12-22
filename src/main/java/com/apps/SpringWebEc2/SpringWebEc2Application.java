2fd0fd3639383e980f9248d36657382bbe059f2f
CEAsis <choloville@gmail.com>
2024-12-22 17:23:02 +0800

    initial

---
+++ src/main/java/com/apps/SpringWebEc2/SpringWebEc2Application.java
@@ -0,0 +1,13 @@
+package com.apps.SpringWebEc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebEc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/apps/SpringWebEc2/SpringWebEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.SpringWebEc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
